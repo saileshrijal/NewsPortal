b255c439e69ed3e623e7a4b88637621f4b772bdf
Sailesh Rijal <saileshrijal1@gmail.com>
2023-05-21 20:21:42 +0545

    Admin Template Added

--- NewsPortal/Program.cs
+++ NewsPortal/Program.cs
@@ -85,4 +85,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+    app.MapControllerRoute(
+    name: "area",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+
 app.Run();
