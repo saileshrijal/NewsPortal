d1e554d97231ef17b1a5cb73469aab2eddae1f2b
Sailesh Rijal <saileshrijal1@gmail.com>
2023-05-26 09:22:46 +0545

    layout clean

--- NewsPortal/Constants/AppConstants.cs
+++ NewsPortal/Constants/AppConstants.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace NewsPortal.Constants
 {
     public static class AppConstants
     {
-        public const string Name = "eLibrary";
+        public const string Name = "News Portal";
         public const string Version = "1.0.0";
+        public const string Developer = "Sailesh Rijal";
     }
 }
